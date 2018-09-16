26b8ee86465035009d6f320b72af073140bf874e
Daniel <danielrsoares2006@gmail.com>
2018-09-16 12:44:59 -0300

    configurando postgre

--- src/main/java/com/drs/AppEvento3/DataConfiguration.java
+++ src/main/java/com/drs/AppEvento3/DataConfiguration.java
@@ -1,19 +1,35 @@
 package com.drs.AppEvento3;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 @Configuration
 public class DataConfiguration {
 
     //Bin de Conexão com o Banco
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+
+    /*
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -37,6 +53,6 @@ public class DataConfiguration {
         adapter.setPrepareConnection(true); //Para o hibernete se conectar, preparando a conexão automaticamente
         return adapter; // Retorna um adapter
     }
-
+*/
 
 }
